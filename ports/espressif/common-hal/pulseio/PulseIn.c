335affea044c3ca8a7d6bd550699990ea1465f20
Sola85 <tschmale85@googlemail.com>
2025-05-30 16:37:18 +0200

    espressif: Try RMT with DMA

--- ports/espressif/common-hal/pulseio/PulseIn.c
+++ ports/espressif/common-hal/pulseio/PulseIn.c
@@ -80,7 +80,7 @@ void common_hal_pulseio_pulsein_construct(pulseio_pulsein_obj_t *self, const mcu
     }
     // We add one to the maxlen version to ensure that two symbols at lease are
     // captured because we may skip the first portion of a symbol.
-    self->raw_symbols_size = MIN(64, maxlen / 2 + 1) * sizeof(rmt_symbol_word_t);
+    self->raw_symbols_size = (maxlen / 2 + 1) * sizeof(rmt_symbol_word_t);
     self->raw_symbols = (rmt_symbol_word_t *)m_malloc_without_collect(self->raw_symbols_size);
     if (self->raw_symbols == NULL) {
         m_free(self->buffer);
@@ -109,7 +109,8 @@ void common_hal_pulseio_pulsein_construct(pulseio_pulsein_obj_t *self, const mcu
         .clk_src = RMT_CLK_SRC_DEFAULT,
         // 2 us resolution so we can capture 65ms pulses. The RMT period is only 15 bits.
         .resolution_hz = 1000000 / 2,
-        .mem_block_symbols = SOC_RMT_MEM_WORDS_PER_CHANNEL,
+        .mem_block_symbols = self->raw_symbols_size,
+        .with_dma = 1
     };
     // If we fail here, the buffers allocated above will be garbage collected.
     CHECK_ESP_RESULT(rmt_new_rx_channel(&config, &self->channel));
